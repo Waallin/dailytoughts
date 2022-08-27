f10738de839cdc929aba96adad7e9c8ac9f8983b
Waallin <viktorwallin@hotmail.se>
2022-08-27 11:08:56 +0200

    dates
    can see dates

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -3,6 +3,7 @@ import { DbService } from '../services/db.service';
 import { environment } from 'src/environments/environment';
 import { SupabaseClient, createClient, User } from '@supabase/supabase-js';
 import { zip } from 'rxjs';
+import { AuthService } from '../services/auth.service';
 
 @Component({
   selector: 'app-main',
@@ -14,18 +15,24 @@ export class MainComponent implements OnInit {
   private supabase: SupabaseClient;
 
 
+  //NgModel inputs
   text: any = '';
   rating: any = '';
+
   error: string = '';
 
+  //items from database, supabase
   db_items: any[] = [];
-  constructor(private db: DbService) {
+
+
+  constructor(private db: DbService, private auth: AuthService) {
     this.supabase = createClient(
       environment.supabaseURL,
       environment.supabaseKey
       );
   }
 
+  //Push-metod 
   async send(text: any, rating: any) {
     if (this.rating > 0 && this.rating < 10) {
       this.db.send((text = this.text), (rating = this.rating))
@@ -44,9 +51,18 @@ export class MainComponent implements OnInit {
 
       console.log(error)
     } else {
+      console.log(this.db_items)
+
       y.forEach((e:any) => {
-        this.db_items.push(e)
-        console.log(e)
+        let obj = {
+          name: 'testing',
+          text: e.text,
+          created_at: e.created_at.slice(0, 10),
+          rating: e.rating
+        }
+
+        this.db_items.push(obj)
+
       })
     }
   }
